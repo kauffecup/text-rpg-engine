9c42087c22f621a54a676016ee9624574033b546
Jonathan Kaufman <jondakauf@gmail.com>
2016-01-20 16:50:55 -0500

    constants!

---
+++ adapters/nodeRed/Constants.js
@@ -0,0 +1,9 @@
+/**
+ * Fun Constants
+ */
+
+export const LOAD_DATA = 'LOAD_DATA';
+export const GAME_DATA = 'GAME_DATA';
+export const USER_INPUT = 'USER_INPUT';
+export const SAVE_DATA = 'SAVE_DATA';
+export const CLEAR_SAVE = 'CLEAR_SAVE';
